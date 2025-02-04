3ffe25911a1f1f36660357425fa8a08b60869aea
David Rodríguez <jacodaro@hotmail.com>
2025-02-04 12:02:38 +0100

    v0.7.1 Cambios menores

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,36 +1,32 @@
-import { useState } from "react"
-import AddCategoria from "./components/AddCategoria"
-import GifGrid from "./components/GifGrid"
-
+import { useState } from "react";
+import AddCategoria from "./components/AddCategoria";
+import GifGrid from "./components/GifGrid";
 
 const GifExpertApp = () => {
-  /* const categorias = ['One Punch', 'Samurai X', 'Dragon Ball'] */
-  const [categorias, setCategorias] = useState([''])
+    /* const categorias = ['One Punch', 'Samurai X', 'Dragon Ball'] */
+    const [categorias, setCategorias] = useState(["One Punch Man"]);
 
-  /*   const agregar = () => {
+    /*   const agregar = () => {
       setCategorias([['Hunter X Hunter'], ...categorias,])
      // setCategorias(cat => [...cat, 'Hunter X Hunter']) 
       console.log(categorias);
     }
     console.log(categorias); */
 
-  return (
-    <>
-      <h1>Buscador de GIFs</h1>
-      <h3>Introduce una o más palabras clave para mostrar una colección de 10 GIFs:</h3>
-      {/* En "<AddCategoria setCategorias={setCategorias} />" se envía la propiedad
-        * "{setCategorias} desde el padre "GifExpertApp" al hijo "AddCategoria"
-      */}
-      <AddCategoria setCategorias={setCategorias} />
-      <hr />
-      <ol>
-        {
-          categorias.map(categoria =>
-            <GifGrid key={categoria} categoria={categoria} />
-          )
-        }
-      </ol>
-    </>
-  )
-}
-export default GifExpertApp
+    return (
+        <>
+            <h2>Gif Finder App</h2>
+            {/* En "<AddCategoria setCategorias={setCategorias} />" se envía la propiedad
+             * "{setCategorias} desde el padre "GifExpertApp" al hijo "AddCategoria"
+             */}
+            <AddCategoria setCategorias={setCategorias} />
+            <hr />
+            <ol>
+                {categorias.map((categoria) => (
+                    <GifGrid key={categoria} categoria={categoria} />
+                ))}
+            </ol>
+        </>
+    );
+};
+export default GifExpertApp;
